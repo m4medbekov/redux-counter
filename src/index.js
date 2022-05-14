2635496ac316327cf127f54f31dd773b1a28101f
m4medbekov <m4medbekov@gmail.com>
2022-05-14 17:10:13 +0300

    Make own createStore + rootReducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import createStore from "./createStore"
+import { rootReducer } from "./redux/rootReducer"
+import './styles.css'
 
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
@@ -6,19 +8,27 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-const store = createStore()
+const store = createStore(rootReducer, 0)
+
+store.subscribe(() => {
+    const state = store.getState()
+    counter.textContent = state
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
 
 addBtn.addEventListener('click', () => {
-
+    store.dispatch({type: 'INCREMENT'})
 })
 
 subBtn.addEventListener('click', () => {
-
+    store.dispatch({type: 'DECREMENT'})
 })
 
 asyncBtn.addEventListener('click', () => {
 
 })
+
 
 themeBtn.addEventListener('click', () => {
     // document.body.classList.toggle('dark')
